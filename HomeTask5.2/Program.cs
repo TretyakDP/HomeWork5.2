3e982213730a9ed2193d509e1300d8c8e152e426
DmitriyTR <skate1233@mail.ru>
2023-11-01 21:46:25 +0300

    Добавьте файлы проекта.

---
+++ HomeTask5.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("Введена пустая или пробельная строка.");
+        }
+        else if (!input.Contains("h"))
+        {
+            Console.WriteLine("В строке нет символа 'h'.");
+        }
+        else
+        {
+            int firstHIndex = input.IndexOf("h");
+            int lastHIndex = input.LastIndexOf("h");
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i != firstHIndex && i != lastHIndex && input[i] == 'h')
+                {
+                    input = input.Remove(i, 1).Insert(i, "H");
+                }
+            }
+
+            Console.WriteLine("Результат: " + input);
+        }
+    }
+}
